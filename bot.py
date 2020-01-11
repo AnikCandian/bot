6faa1b27f5c73a840a5002da7eb21462516f5c2e
AnikCandian <38167252+AnikCandian@users.noreply.github.com>
2020-01-10 17:12:04 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -119,4 +119,4 @@ async def help(ctx, command="None"):
 async def warn(ctx, person, reason="None"):
 	print("lol")
 
-bot.run("NjYzNjgzNTcwODA3NjAzMjIw.XhkR0Q.PnGYrNevP47tGAmHLey3n_fNjEE")
+bot.run("NjYzNjgzNTcwODA3NjAzMjIw.XhkSvw.30_AZHuDLTF52ybQKdJ37HRwo-Q")
