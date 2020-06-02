f2070a6b2e389806357a9e4e4b68f2c614c6aa18
AnikCandian <38167252+AnikCandian@users.noreply.github.com>
2020-06-02 12:09:23 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 
 /* Initialize the required constants. "Discord" gets the variables, functions and such for the discord.js library, while
 client makes a new object from the discord.js library. */
-/*
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -16,20 +16,5 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
-*/
-module.exports = {
-  const Discord = require('discord.js');
-  const client = new Discord.Client();
 
-
-  //  Whenever the client is on, the console will log "Logged in as (insert bot name here)" if successfully started and ready.
-  client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
-
-  client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
-   }
-  });
-}
+client.run(process.env.TOKEN) // Change this to whatever you want.
